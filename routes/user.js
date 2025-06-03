2a4a772d66b178ab99ce17db3776a2c7cd9604f6
jaishruti <shrutijaiswal223@gmail.com>
2025-06-03 14:43:07 +0530

    setup middleware routes models added

--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const router = express.Router();
+
+const { login, signup } = require("../controllers/Auth");
+
+router.post("/login", login);
+router.post("/signup", signup);
+
+modules.exports = router;
